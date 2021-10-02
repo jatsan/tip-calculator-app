b6d606dae9fe22ab9c3d2bd482e89e51ec559272
James Terry <jaterry.dev@gmail.com>
2021-10-02 03:19:54 -0400

    dev: added postcss and babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const ESLintPlugin = require('eslint-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 let buildMode = 'development'
 
@@ -32,8 +34,34 @@ module.exports = {
         test: /\.(ico|png|jpe?g|gif|svg)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
     ],
   },
   // plugins
-  plugins: [new ESLintPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, './src/template.html'),
+      favicon: path.resolve(__dirname, './src/images/favicon-32x32.png'),
+    }),
+    new ESLintPlugin(),
+  ],
 }
