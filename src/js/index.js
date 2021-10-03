7f0e7d26eafd12a777d370b75a1c12b2e7a2049f
James Terry <jaterry.dev@gmail.com>
2021-10-02 22:01:01 -0400

    feat: added ui for tip calc input

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,13 @@
 import '../scss/style.scss'
 import logo from '../images/logo.svg'
+import dollar from '../images/icon-dollar.svg'
+import person from '../images/icon-person.svg'
+import { addHeaderLogo, addDollarImg, addPersonImg } from './util'
 
-const imgLogo = document.createElement('img')
-imgLogo.src = logo
-imgLogo.alt = 'Splitter Tip Calculator'
-const header = document.querySelector('header')
-header.append(imgLogo)
+const initApp = () => {
+  addHeaderLogo(logo)
+  addDollarImg(dollar)
+  addPersonImg(person)
+}
+
+initApp()
--- src/js/util.js
+++ src/js/util.js
@@ -1,1 +1,24 @@
+const addHeaderLogo = (img) => {
+  const imgLogo = document.createElement('img')
+  imgLogo.src = img
+  imgLogo.alt = 'Splitter Tip Calculator'
+  const header = document.querySelector('header')
+  header.append(imgLogo)
+}
 
+const addDollarImg = (img) => {
+  const imgDollar = document.createElement('img')
+  imgDollar.src = img
+  imgDollar.ariaHidden = 'true'
+  const dollarDiv = document.querySelector('.dollar_bill')
+  dollarDiv.append(imgDollar)
+}
+
+const addPersonImg = (img) => {
+  const imgPerson = document.createElement('img')
+  imgPerson.src = img
+  const personDiv = document.querySelector('.person')
+  personDiv.append(imgPerson)
+}
+
+export { addHeaderLogo, addDollarImg, addPersonImg }
