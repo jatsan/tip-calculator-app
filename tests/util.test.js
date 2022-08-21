8d4abf23c2e14ae2eb44ab47e5ef27612138f57c
James Terry <jaterry.dev@gmail.com>
2022-08-20 22:22:27 -0400

    Added and refactored test cases

--- tests/util.test.js
+++ tests/util.test.js
@@ -1,12 +1,24 @@
-const checkValidity = require('../src/js/util')
+const valid = require('../src/js/util')
 
 describe('checkValidity function', () => {
   test('should return false if zero', () => {
-    const result = !checkValidity(0)
+    const result = valid(0)
     expect(result).toBe(false)
   })
   test('should return false if blank', () => {
-    const result = !checkValidity('')
+    const result = valid('')
     expect(result).toBe(false)
+  })
+  test('should return false if negative', () => {
+    const result = valid(-56)
+    expect(result).toBe(false)
+  })
+  test('should return false if not whole number', () => {
+    const result = valid(5.5)
+    expect(result).toBe(false)
+  })
+  test('should return true if num is positive whole number', () => {
+    const result = valid(5)
+    expect(result).toBe(true)
   })
 })
